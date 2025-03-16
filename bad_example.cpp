414ef018875b170ae54906283ce2985156914cfe
blazie <jayvc1703@gmail.com>
2025-03-16 07:13:31 +0000

    Fixed clang-format issues

--- bad_example.cpp
+++ bad_example.cpp
@@ -1,6 +1,7 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int x=10;cout<<"Value of x: "<<x<<endl;
-    return 0;}
-
+int main() {
+  int x = 10;
+  cout << "Value of x: " << x << endl;
+  return 0;
+}
--- good_example.cpp
+++ good_example.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 
 int main() {
-    int x = 10;
-    cout << "Value of x: " << x << endl;
-    return 0;
+  int x = 10;
+  cout << "Value of x: " << x << endl;
+  return 0;
 }
-
