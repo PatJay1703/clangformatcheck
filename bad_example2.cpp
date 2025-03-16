69c78628d6e3acb0ba3402fdf09d679fbf7ac3ac
blazie <jayvc1703@gmail.com>
2025-03-16 07:29:30 +0000

    after workflow showed error , fixed it using clang formatgit status

--- bad_example2.cpp
+++ bad_example2.cpp
@@ -1,4 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main() {int a=10;int b=20; if(a<b){cout<<"A is smaller than B"<<endl;}else{cout<<"B is smaller than A"<<endl;}return 0;}
-
+int main() {
+  int a = 10;
+  int b = 20;
+  if (a < b) {
+    cout << "A is smaller than B" << endl;
+  } else {
+    cout << "B is smaller than A" << endl;
+  }
+  return 0;
+}
