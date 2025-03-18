47b3760655e3126e659c97603efc46362cf2a435
blazie <jayvc1703@gmail.com>
2025-03-18 08:02:05 +0000

    changed bothfail.cpp

--- bothfail.cpp
+++ bothfail.cpp
@@ -1,13 +1,48 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 
-void foo(int x, int y) {
-    std::vector<int> v;
-    for (int i = 0; i < 10; i++) {
-        v.push_back(i);
-    }
-    if (x>y) {std::cout << "x is greater than y" << std::endl;} else {std::cout << "y is greater than x" << std::endl;}
+using namespace std;
+
+class Person{
+private:
+    string name;
+    int age;
+public:
+    Person(string n, int a): name(n), age(a){};
+    void display() { cout << "Name: " << name << ", Age: " << age << endl; }
+};
+
+void sortNames(vector<Person> &people){
+    sort(people.begin(), people.end(), [](const Person &a, const Person &b){
+        return a.name < b.name;
+    });
 }
 
-int main() {int a=10,b=20;foo(a,b);return 0;}
+int main()
+{
+  vector<Person> people;
+  people.push_back(Person("John", 30));
+  people.push_back(Person("Jane", 25));
+  people.push_back(Person("Mike", 35));
+  people.push_back(Person("Alice", 28));
+  
+  sortNames(people);
+  
+  for(auto &p: people)
+  {
+    p.display();
+  }
+  
+  int x= 10,y=20,z=30;
+  if(x>y&&y<z){cout<<"x is greater than y and y is less than z\n";} else {cout<<"Condition failed\n";}
+
+  // This part below has some issues that would fail clang-tidy
+  int largeArray[10000];
+  for(int i = 0; i < 10000; ++i) {largeArray[i]=i;}
+  
+  // Some inline code to test failure
+  if(x==y){ cout<<"Equal"; return 0;}
+  return 0;
+}
 
