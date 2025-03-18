6ab2dc6fc4cfdbac4ed7dd34de564b5ad96271c0
blazie <jayvc1703@gmail.com>
2025-03-18 10:58:06 +0000

    changed bfail.cpp

--- bfail.cpp
+++ bfail.cpp
@@ -9,6 +9,6 @@ void foo(int x, int y) {
     if (x>y) {std::cout << "x is greater than y" << std::endl;} else {std::cout << "y is greater than x" << std::endl;}
 }
 
-int main() {int a=10,b=20;foo(a,b);return 0;}
+int main(){int a=10,b=20;foo(a,b);return 0;}
 
 
