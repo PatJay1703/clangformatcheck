d492b8dd292182d52479d14d477df2c5cdeaa1eb
blazie <jayvc1703@gmail.com>
2025-03-26 05:54:52 +0000

    added new tcs

---
+++ testcases/tc11.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void foo(int x, int y)
+{
+    if(x>y){std::cout<<"x is greater than y"<<std::endl;}else{std::cout<<"y is greater than x"<<std::endl;} 
+}
+
+
+int main() {
+  int a=10,b=20;
+  foo(a,b);
+    return 0;
+}
+
+
