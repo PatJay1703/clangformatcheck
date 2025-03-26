c378561b0f464eb1a911339f0e2d52cd74f8afa1
blazie <jayvc1703@gmail.com>
2025-03-26 09:02:52 +0000

    added ch in t1

--- testcases/t1.cpp
+++ testcases/t1.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Hello, World!" << std::endl;
+    cout <<"Hello, World!"<<std::endl;
+   if(5>4){cout<<"hello world";}
     return 0;
 }
 
