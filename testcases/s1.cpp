78f38b97557667e89d607cadecf588b5dc77d3c5
blazie <jayvc1703@gmail.com>
2025-04-02 05:50:37 +0000

    added new testcase

---
+++ testcases/s1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+   int a,b,c;
+  cout<<"helloworld";
+  if(a==b) cout<<"a is greater";
+  if(a<b)cout<<"a is smaller than b";
+  return 0;
+}
