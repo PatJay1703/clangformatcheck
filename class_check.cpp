c81b742fed2758e3f7f1716367d3a457b3be6572
blazie <jayvc1703@gmail.com>
2025-04-04 07:36:42 +0000

    class check tc

---
+++ class_check.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+// Class with missing documentation
+class TextFileReader {
+public:
+    void OpenFile() {  // Function name starts with uppercase (violation)
+        std::cout << "Opening file..." << std::endl;
+    }
+};
+
+enum valueKind {  // Enum name starts with lowercase (violation)
+    VALUE1,
+    VALUE2
+};
+
+int Leader = 0;  // Variable name starts with uppercase (violation)
+
+void Getitems() {  // Function name starts with uppercase (violation)
+    std::cout << "Getting items..." << std::endl;
+}
+
+int main() {
+    TextFileReader reader;
+    reader.OpenFile();
+    Getitems();
+    return 0;
+}
