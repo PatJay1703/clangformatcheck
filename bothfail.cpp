c32ae4f415b07407f81f6b7e6df20ff60900dbda
Jay Satish Kumar Patel <kumarpat@pe28vega.hpc.amslabs.hpecorp.net>
2025-04-09 02:37:51 -0500

    added changes

--- bothfail.cpp
+++ bothfail.cpp
@@ -19,30 +19,22 @@ void sortNames(vector<Person> &people){
     });
 }
 
+int sum(int a, int b) {
+  if (a > b) {
+      return a;
+  }
+  // Missing return statement for the case when a <= b
+}
 int main()
 {
-  vector<Person> people;
-  people.push_back(Person("John", 30));
-  people.push_back(Person("Jane", 25));
-  people.push_back(Person("Mike", 35));
-  people.push_back(Person("Alice", 28));
-  
-  sortNames(people);
-  
-  for(auto &p: people)
-  {
-    p.display();
-  }
-  
-  int x= 10,y=20,z=30;
-  if(x>y&&y<z){cout<<"x is greater than y and y is less than z\n";} else {cout<<"Condition failed\n";}
+ 
 
-  // This part below has some issues that would fail clang-tidy
-  int largeArray[10000];
-  for(int i = 0; i < 10000; ++i) {largeArray[i]=i;}
-  
-  // Some inline code to test failure
-  if(x==y){ cout<<"Equal"; return 0;}
-  return 0;
+
+
+int main() {
+    std::cout << sum(10, 5) << std::endl;
+    return 0;
+}
+
 }
 
