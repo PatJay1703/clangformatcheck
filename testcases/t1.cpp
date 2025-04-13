ee4ecb423f8fd5187a98ecc94dc62006d465f5d5
blazie <jayvc1703@gmail.com>
2025-04-13 11:29:54 +0000

    added changes

--- testcases/t1.cpp
+++ testcases/t1.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 using namespace std;
+void foo(vector<int>& vec)
+{
+for(int i=0;i<vec.size();i++)
+{
+if(vec[i]%2==0)
+{
+cout<<vec[i]<<" is even"<<endl;
+}
+else
+{
+cout<<vec[i]<<" is odd"<<endl;
+}
+}
+}
 int main(){
 int x=  42;
    if(x>0){
